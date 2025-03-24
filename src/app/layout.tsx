b994bc30cd139f41ce458ed52ca1335b5ac373be
limjcp <2022_cete_limjcp@online.htcgsc.edu.ph>
2025-03-24 16:23:51 +0800

    fix: Update HOST_IP in docker-compose and add archived role to schema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,15 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "GSCWD Queueing System",
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <SessionProvider>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
         </body>
